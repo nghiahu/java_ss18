deae45b728f0e94a36e2f8c2c2f5080393ea78df
CanhVeTaiGia <nghia120425@gmail.com>
2025-03-25 12:02:55 +0700

    update

--- src/baitap/Bai8.java
+++ src/baitap/Bai8.java
@@ -23,10 +23,46 @@ public class Bai8 {
         System.out.print("Nhập vào một số nguyên: ");
         int n = Integer.parseInt(scanner.nextLine());
         StringBuilder romanNumber = new StringBuilder();
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            while (n >= entry.getKey()) {
-                romanNumber.append(entry.getValue());
-                n -= entry.getKey();
+        while (n > 0) {
+            if(n >= 1000){
+                romanNumber.append("M");
+                n -= 1000;
+            } else if (n >= 900) {
+                romanNumber.append("CM");
+                n -= 900;
+            }else if (n >= 500) {
+                romanNumber.append("D");
+                n -= 400;
+            } else if (n >= 400) {
+                romanNumber.append("CD");
+                n -= 400;
+            }else if (n >= 100) {
+                romanNumber.append("C");
+                n -= 100;
+            }else if (n >= 90) {
+                romanNumber.append("XC");
+                n -= 90;
+            }else if (n >= 50) {
+                romanNumber.append("L");
+                n -= 50;
+            }else if (n >= 40) {
+                romanNumber.append("XL");
+                n -= 40;
+            }else if (n >= 10) {
+                romanNumber.append("X");
+                n -= 10;
+            }else if (n >= 9) {
+                romanNumber.append("IX");
+                n -= 9;
+            }else if (n >= 5) {
+                romanNumber.append("V");
+                n -= 5;
+            }else if (n >= 4) {
+                romanNumber.append("IV");
+                n -= 4;
+            }else if (n >= 1) {
+                romanNumber.append("I");
+                n -= 1;
             }
         }
         System.out.println("Số La Mã: " + romanNumber);
